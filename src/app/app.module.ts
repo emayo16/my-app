cbefc79b872d2c23abc05d33ce6322dee4a6df56
Ethan D. Mayo <emayo16@gmail.com>
2017-11-04 17:48:25 -0400

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatExpansionModule, MatButtonModule, MatCheckboxModule, MatGridListModule, MatToolbarModule, MatCardModule, MatInputModule, MatIconModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HeroDetailComponent } from './hero/hero-detail/hero-detail.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroDetailComponent,
+    DashboardComponent,
+    HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatExpansionModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatGridListModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatInputModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
